52b32c29268ad367c1e8a4453e0e309c7aa6074a
Reynaldo Abreu <reynaldodadilson@hotmail.com>
2022-08-04 23:43:10 -0300

    Criado o model e repository

---
+++ parking-control/src/main/java/com/api/parkingcontrol/model/ParkingSpotModel.java
@@ -0,0 +1,39 @@
+package com.api.parkingcontrol.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.rmi.server.UID;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+public class ParkingSpotModel{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingSpotnumber;
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlateCar;
+    @Column(nullable = false, length = 70)
+    private String brandCar;
+    @Column(nullable = false, length = 70)
+    private String modelCar;
+    @Column(nullable = false, length = 70)
+    private String colorCar;
+    @Column(nullable = false)
+    private LocalDateTime registationDate;
+    @Column(nullable = false, length = 130)
+    private String Ownername;
+    @Column(nullable = false, length = 30)
+    private String apartment;
+    @Column(nullable = false, length = 30)
+    private String block;
+
+
+}
